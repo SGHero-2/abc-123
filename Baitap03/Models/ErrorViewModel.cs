a6d0f1fb427fb61e522b9de315ef88f70c9772ff
SGHero-2 <tungp2024@gmail.com>
2024-08-27 14:00:30 +0700

    Delete Baitap03 directory

--- Baitap03/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Baitap03.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
