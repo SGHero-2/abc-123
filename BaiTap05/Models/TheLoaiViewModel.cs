a09a504956582fab060e73e26c744c0444a39af7
Thanh Tùng <tungp2016@gmail.com>
2024-09-06 15:27:49 +0700

    Create BaiTap05

--- BaiTap05/Models/TheLoaiViewModel.cs
+++ BaiTap05/Models/TheLoaiViewModel.cs
@@ -5,7 +5,7 @@ namespace BaiTap05.Models
     public class TheLoaiViewModel : Controller
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public IActionResult Index()
         {
             return View();
