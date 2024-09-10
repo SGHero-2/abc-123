80f10d2e2d9d0984467f0c3418bd64a45d42eaec
Thanh Tùng <tungp2016@gmail.com>
2024-09-10 15:12:50 +0700

    Đã nộp bài ngày 10/9

---
+++ BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,31 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taiKhoan)
+        {
+            if (taiKhoan.Username != null)
+            {
+                return Content(taiKhoan.Username);
+            }
+            return View(taiKhoan);
+        }
+        public IActionResult BaiTap2()
+        {
+            var sanpham = new SanPhamViewModel()
+            {
+                TenSP = "Trà",
+                GiaBan = 1000,
+                AnhMoTa = "/images/iphone-screen.png"
+            };
+            return View(sanpham);
+        }
+    }
+}
